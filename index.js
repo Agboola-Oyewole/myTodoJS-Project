bd2b07d421ab13f0b6739b6a38a2307675571076
Oyewole Agboola <121974499+Agboola-Oyewole@users.noreply.github.com>
2024-03-31 17:43:16 +0100

    Update index.js with google link

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "https://mytodojs-project.onrender.com/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
@@ -299,6 +299,3 @@ passport.deserializeUser((user, cb) => {
   cb(null, user);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
